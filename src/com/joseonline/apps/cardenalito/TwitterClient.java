fad360b9e678cb1f5b03dd2c92c4bd23a4d49c3a
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-06-21 15:14:47 -0700

    breakdown api key/secret to a different class

--- src/com/joseonline/apps/cardenalito/TwitterClient.java
+++ src/com/joseonline/apps/cardenalito/TwitterClient.java
@@ -1,9 +1,6 @@
 
 package com.joseonline.apps.cardenalito;
 
-import org.scribe.builder.api.Api;
-import org.scribe.builder.api.TwitterApi;
-
 import android.content.Context;
 
 import com.codepath.oauth.OAuthBaseClient;
@@ -11,23 +8,20 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 public class TwitterClient extends OAuthBaseClient {
-    public static final Class<? extends Api> REST_API_CLASS = TwitterApi.class;
-    public static final String REST_URL = "https://api.twitter.com/1.1";
-    public static final String REST_CONSUMER_KEY = "SOME_KEY";
-    public static final String REST_CONSUMER_SECRET = "SOME_SECRET";
-    public static final String REST_CALLBACK_URL = "oauth://cardenalito";
 
     public TwitterClient(Context context) {
-        super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET,
-                REST_CALLBACK_URL);
+        super(context, TwitterClientSettings.REST_API_CLASS, TwitterClientSettings.REST_URL,
+                TwitterClientSettings.REST_CONSUMER_KEY,
+                TwitterClientSettings.REST_CONSUMER_SECRET,
+                TwitterClientSettings.REST_CALLBACK_URL);
     }
 
     public void getHomeTimeline(AsyncHttpResponseHandler handler) {
         String apiUrl = getApiUrl("/statuses/home_timeline.json");
-        
+
         RequestParams params = new RequestParams();
         params.put("since_id", "1");
-        
+
         client.get(apiUrl, params, handler);
     }
 }
