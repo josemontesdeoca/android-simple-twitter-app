d91e18ece3dd35c8401a14c7d544051d69bb51ad
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-06-21 21:21:40 -0700

    Add post tweet method to client library

--- src/com/joseonline/apps/cardenalito/TwitterClient.java
+++ src/com/joseonline/apps/cardenalito/TwitterClient.java
@@ -10,9 +10,11 @@ import com.loopj.android.http.RequestParams;
 public class TwitterClient extends OAuthBaseClient {
     private static final String COUNT_PARAMETER = "count";
     private static final String MAX_ID_PARAMETER = "max_id";
+    private static final String STATUS_PARAMETER = "status";
 
     private static final String HOME_TIMELINE_PATH = "/statuses/home_timeline.json";
     private static final String ACCOUNT_VERIFY_CREDENTIALS_PATH = "/account/verify_credentials.json";
+    private static final String POST_TWEET_PATH = "/statuses/update.json";
 
     public TwitterClient(Context context) {
         super(context, TwitterClientSettings.REST_API_CLASS, TwitterClientSettings.REST_URL,
@@ -37,5 +39,14 @@ public class TwitterClient extends OAuthBaseClient {
     public void getAuthenticatedUser(AsyncHttpResponseHandler handler) {
         String apiUrl = getApiUrl(ACCOUNT_VERIFY_CREDENTIALS_PATH);
         client.get(apiUrl, handler);
+    }
+    
+    public void postTweet(String tweet, AsyncHttpResponseHandler handler) {
+        String apiUrl = getApiUrl(POST_TWEET_PATH);
+        
+        RequestParams params = new RequestParams();
+        params.put(STATUS_PARAMETER, tweet);
+        
+        client.post(apiUrl, params, handler);
     }
 }
