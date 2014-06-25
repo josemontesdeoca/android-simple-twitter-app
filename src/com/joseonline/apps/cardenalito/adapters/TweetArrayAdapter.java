dec29e26a1cae950ae5b817e2f45e5f5d4347a5e
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-06-24 22:40:38 -0700

    Add media view on timeline

--- src/com/joseonline/apps/cardenalito/adapters/TweetArrayAdapter.java
+++ src/com/joseonline/apps/cardenalito/adapters/TweetArrayAdapter.java
@@ -40,6 +40,7 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
         TextView tvScreenName = (TextView) v.findViewById(R.id.tvScreenName);
         TextView tvCreatedAt = (TextView) v.findViewById(R.id.tvCreatedAt);
         TextView tvTweetBody = (TextView) v.findViewById(R.id.tvTweetBody);
+        ImageView ivMediaEntity = (ImageView) v.findViewById(R.id.ivMediaEntity);
 
         ImageLoader imageLoader = ImageLoader.getInstance();
 
@@ -50,6 +51,12 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
         tvCreatedAt.setText(DateUtil.getRelativeTimeAgo(tweet.getCreateAt(),
                 DateUtil.TWITTER_TIME_FORMAT));
         tvTweetBody.setText(tweet.getBody());
+        
+        if (tweet.getMediaUrl() != null) {
+            ivMediaEntity.setImageResource(android.R.color.transparent);
+            ivMediaEntity.setVisibility(View.VISIBLE);
+            imageLoader.displayImage(tweet.getMediaUrlThumb(), ivMediaEntity);
+        }
 
         return v;
     }
