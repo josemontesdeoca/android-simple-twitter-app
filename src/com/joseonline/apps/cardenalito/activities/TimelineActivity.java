ff588ded831fdcd35a9bbcb7205e6dabc84ba2a1
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2014-06-28 12:26:57 -0700

    Add tabs for Home and Mentions tweets.
    - Users can switch between timeline and mentions views using tabs

--- src/com/joseonline/apps/cardenalito/activities/TimelineActivity.java
+++ src/com/joseonline/apps/cardenalito/activities/TimelineActivity.java
@@ -3,6 +3,8 @@ package com.joseonline.apps.cardenalito.activities;
 
 import org.json.JSONObject;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -12,7 +14,10 @@ import android.view.MenuItem;
 
 import com.joseonline.apps.cardenalito.CardenalitoApplication;
 import com.joseonline.apps.cardenalito.R;
+import com.joseonline.apps.cardenalito.fragments.HomeTimelineFragment;
+import com.joseonline.apps.cardenalito.fragments.MentionsTimelineFragment;
 import com.joseonline.apps.cardenalito.fragments.TweetsListFragment;
+import com.joseonline.apps.cardenalito.listeners.FragmentTabListener;
 import com.joseonline.apps.cardenalito.models.Tweet;
 import com.joseonline.apps.cardenalito.models.User;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -30,9 +35,40 @@ public class TimelineActivity extends FragmentActivity {
         setContentView(R.layout.activity_timeline);
 
         fragmentTweetsList = (TweetsListFragment) getSupportFragmentManager().findFragmentById(
-                R.id.fragment_timeline);
+                R.id.flContainer);
 
         getAuthenticatedUser();
+
+        setupTabs();
+    }
+
+    private void setupTabs() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(true);
+
+        Tab tab1 = actionBar
+                .newTab()
+                .setText("Home")
+                // .setIcon(R.drawable.ic_home)
+                .setTag("HomeTimelineFragment")
+                .setTabListener(
+                        new FragmentTabListener<HomeTimelineFragment>(R.id.flContainer, this,
+                                "home", HomeTimelineFragment.class));
+
+        actionBar.addTab(tab1);
+        actionBar.selectTab(tab1);
+
+        Tab tab2 = actionBar
+                .newTab()
+                .setText("Mentions")
+                // .setIcon(R.drawable.ic_mentions)
+                .setTag("MentionsTimelineFragment")
+                .setTabListener(
+                        new FragmentTabListener<MentionsTimelineFragment>(R.id.flContainer, this,
+                                "mentions", MentionsTimelineFragment.class));
+
+        actionBar.addTab(tab2);
     }
 
     @Override
